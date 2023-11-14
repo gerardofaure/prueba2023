f562e2c0467e4495581b5e344047965f338b01b9
gerardofaure <gerardofaure@gmail.com>
2023-11-14 18:13:10 -0300

    primer commit

---
+++ clase0.js
@@ -0,0 +1,19 @@
+let nombre="Gerardo";
+
+let edad="44";
+
+let precio=500;
+
+let series = ["Breaking Bad" , "Pricipe del rap" , "TBBT"];
+
+let peliculas = ["Terminator" , "Forest Gump" , "Volver al Futuro"];
+
+console.log(nombre, " | " + edad, " | " + precio, " | " + series, " | " + peliculas );
+
+edad++
+
+series.push ("Los Pitufos");
+
+peliculas.push("Mad Max");
+
+console.log(nombre, " | " + edad, " | " + precio, " | " + series, " | " + peliculas);
